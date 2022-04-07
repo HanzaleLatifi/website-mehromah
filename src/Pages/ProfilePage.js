e80dca429435e327a26b502740d9b1af65f71ddc
hanzale latifi <75186984+HanzaleLatifi@users.noreply.github.com>
2022-04-07 21:39:51 +0430

    add profilePage

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import image from "../assets/images.png"
 function ProfilePage() {
   return (
-    <div className='text-6xl mt-16 text-center'>پروفایل</div>
+    <section className='text-2xl mt-8 flex w-4/5 p-2 justify-around  bg-pink-700 mx-auto rounded-3xl'>
+      <div className='flex flex-col p-4 w-2/3 bg-gray-100 ml-8 rounded-3xl ' >
+            <div className=' text-center' >
+                <p>اطلاعات</p>
+            </div>
+            <div className=' text-center' >
+                <p>سفارشات</p>
+            </div>
+        </div>
+        <div className='flex flex-col bg-gray-100 p-4 w-1/3 items-center rounded-3xl'>
+            <div className='w-40 h-40  p-2 mb-8  '>
+              <img src={image} className="text-center"/>
+            </div >
+            <p >نام کاربری : </p>
+            <p>ایمیل :</p>
+        </div>
+
+    </section>
   )
 }
 
